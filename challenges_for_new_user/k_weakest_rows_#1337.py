b9184f38886aedfbf8b926b8b907cafc5f6ed835
Preethi <preethi13m@gmail.com>
2022-11-22 12:13:23 +0530

    Runtime: 238ms, Memory Usage:14.3MB

---
+++ challenges_for_new_user/k_weakest_rows_#1337.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        arr=[]
+        arr1=[]
+        for x in range(len(mat)):
+            arr1.append((x,sum(mat[x])))
+        arr1.sort(key= lambda x:x[1])
+        print(arr1)
+        for x in range(k):
+            arr.append(arr1[x][0])
+        return arr
