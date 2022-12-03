987f4864744bba0cbc14277b187a841a6ebaeeef
Preethi <preethi13m@gmail.com>
2022-12-03 13:34:19 +0530

    Runtime: 68ms, Memory Usage:15.2MB

---
+++ easy/lar_odd_str#1903.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for x in range(len(num)-1,-1,-1):
+            if(int(num[x])%2!=0):
+                return num[:x+1]
+        return ""
