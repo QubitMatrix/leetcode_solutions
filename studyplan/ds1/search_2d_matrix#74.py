273bc4ed1f35e976de0b497ab90926d8f5589eab
Preethi <preethi13m@gmail.com>
2022-12-26 20:26:14 +0530

    Runtime: 40ms, Memory Usage:14.4MB
    Time-O(logn), Space-O(1)

--- studyplan/ds1/search_2d_matrix#74.py
+++ studyplan/ds1/search_2d_matrix#74.py
@@ -4,5 +4,15 @@ class Solution:
         row=len(matrix)
         for x in range(row):
             if(target<=matrix[x][col-1]):
-                return target in matrix[x]
+                l=0
+                h=col
+                while(l<=h):
+                    mid=(l+h)//2
+                    if(target==matrix[x][mid]):
+                        return True
+                    elif(target<matrix[x][mid]):
+                        h=mid-1
+                    else:
+                        l=mid+1
+                return False
         return False
