a9f1efecad6ab522b10b371b494df9301b473a74
Preethi <preethi13m@gmail.com>
2022-12-27 11:09:41 +0530

    Runtime: 69ms, Memory Usage:14.1MB
    Time-O(n), Space-O(1)

--- challenges_for_new_user/ransom_note_#383.py
+++ challenges_for_new_user/ransom_note_#383.py
@@ -1,11 +1,20 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        print(set(ransomNote).intersection(set(magazine)),set(magazine))
         if(set(ransomNote).intersection(set(magazine))!=set(ransomNote)):
-            print("1")
             return False
-        else:
-            for x in magazine:
-                if(ransomNote.count(x)>magazine.count(x)):
-                    return False
-            return True
+        dic1={}
+        dic2={}
+        for x in ransomNote:
+            if(x in dic1.keys()):
+                dic1[x]+=1
+            else:
+                dic1[x]=1
+        for x in magazine:
+            if(x in dic2.keys()):
+                dic2[x]+=1
+            else:
+                dic2[x]=1
+        for x in dic1.keys():
+            if(dic1[x]>dic2[x]):
+                return False
+        return True
