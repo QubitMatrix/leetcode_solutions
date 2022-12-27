5c79abc087525e62a3b48facaaa67fcad820eb33
Preethi <preethi13m@gmail.com>
2022-12-27 11:37:55 +0530

    Runtime: 66ms, Memory Usage:14.5MB
    Time-O(n), Space-O(1)

--- easy/valid_anagram#242.py
+++ easy/valid_anagram#242.py
@@ -1,5 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s=sorted(s)
-        t=sorted(t)
-        return s==t
+        dic1={}
+        dic2={}
+        for x in s:
+            if(x in dic1.keys()):
+                dic1[x]+=1
+            else:
+                dic1[x]=1
+        for x in t:
+            if( x in dic2.keys()):
+                dic2[x]+=1
+            else:
+                dic2[x]=1
+        if(dic1.keys()!=dic2.keys()):
+            return False
+        for x in dic1.keys():
+            if(dic1[x]!=dic2[x]):
+                return False
+        return True
