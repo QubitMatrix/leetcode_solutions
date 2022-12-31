79d1ec8f4d975446fa8096a2b008076f2c9697ce
Preethi <preethi13m@gmail.com>
2022-12-31 12:34:28 +0530

    Runtime:27ms, Memory Usage:13.9MB
    Follow up answer(in one pass)

--- studyplan/algo1/rem_nth_end#19.py
+++ studyplan/algo1/rem_nth_end#19.py
@@ -5,23 +5,22 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        arr=[]
         count=0
         dhead=head
         while(dhead!=None):
             count+=1
+            arr.append(dhead)
             dhead=dhead.next
         pos=count-n
-        if(pos==0 and count==1):
+        if(count==1 and pos==0):
             return None
-        prev=None
-        dhead=head
-        count=0
-        while(count<pos):
-            prev=dhead
-            dhead=dhead.next
-            count+=1
-        if(not prev):
-            head=head.next
+        elif(pos==0):
+            return head.next
         else:
-            prev.next=dhead.next
-        return head
+            t=arr[pos-1]
+            try:
+                t.next=arr[pos+1]
+            except:
+                t.next=None
+            return head
