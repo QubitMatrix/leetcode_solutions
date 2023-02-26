195faaa0ff7061c81360ec05d10675b0194e2667
Preethi <preethi13m@gmail.com>
2023-02-26 22:48:25 +0530

    Run commit -mRun

---
+++ daily_challenges/feb/best_time_stock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minn=99999
+        maxprofit=0
+        for x in range(len(prices)):
+            if(prices[x]<minn):
+                minn=prices[x]
+            if(prices[x]-minn>maxprofit):
+                maxprofit=prices[x]-minn
+        return maxprofit
