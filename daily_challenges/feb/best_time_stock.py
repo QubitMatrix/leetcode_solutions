f99e1d5320897b37be1146a21a818d92ddabb7ee
Preethi <preethi13m@gmail.com>
2023-02-26 22:51:33 +0530

    Runtime:942ms, Memory Usage:25MB

--- daily_challenges/feb/best_time_stock.py
+++ daily_challenges/feb/best_time_stock.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int])-> int:
         minn=99999
         maxprofit=0
         for x in range(len(prices)):
