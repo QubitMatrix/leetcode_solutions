3bce1efa077dcd115164490c855a735c50dba237
QubitMatrix <preethi13m@gmail.com>
2023-04-24 11:26:30 +0530

    Runtime:25ms, Memory Usage:13.9MB

--- daily_challenges/april/last_stone_weight#1046.py
+++ daily_challenges/april/last_stone_weight#1046.py
@@ -1,9 +1,12 @@
+import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        #heapq implements min heap we need max, use negative values instead
+        stones=[-1*x for x in stones]
         while(len(stones)>1):
-            stones.sort()
-            x=stones.pop()
-            y=stones.pop()
+            heapq.heapify(stones)
+            x=heapq.heappop(stones)
+            y=heapq.heappop(stones)
             if(x!=y):
-                stones.append(int(math.fabs(x-y)))
-        return stones[0] if len(stones)==1 else 0
+                heapq.heappush(stones,-1*int(math.fabs(x-y)))
+        return -1*stones[0] if len(stones)==1 else 0
