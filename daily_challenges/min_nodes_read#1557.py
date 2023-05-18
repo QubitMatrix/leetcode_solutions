0e0e160257f3ef44b4c99cb88c97d7f54226c99f
QubitMatrix <preethi13m@gmail.com>
2023-05-18 22:39:13 +0530

    Time:O(n), Space:O(n)

--- daily_challenges/min_nodes_read#1557.py
+++ daily_challenges/min_nodes_read#1557.py
@@ -1,16 +1,8 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        arr=[]
-        flag=0
-        mat=[[0 for x in range(n)] for y in range(n)]
+        s=set()
+        v=set(range(n))
         for x,y in edges:
-            mat[x][y]=1
-        for x in range(n):
-            for y in range(n):
-                if(mat[y][x]):
-                    flag=1
-                    break
-            if(not flag):
-                arr.append(x)
-            flag=0
-        return arr
+            s.add(y)
+        return list(v-s)
+        
