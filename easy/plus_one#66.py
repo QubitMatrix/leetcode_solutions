3bb6571521dc8badf34cafdadf56fac86bb88ea8
QubitMatrix <preethi13m@gmail.com>
2023-06-21 22:15:47 +0530

    Runtime:13ms, Memory Usage:16.2MB

--- easy/plus_one#66.py
+++ easy/plus_one#66.py
@@ -1,20 +1,5 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if(digits[-1]!=9):
-            digits[-1]+=1
-        else:
-            i=len(digits)-1
-            while(i>-2):
-                if(i==-1):
-                    temp=digits.copy()
-                    digits=[]
-                    digits.append(1)
-                    digits.extend(temp)
-                    break
-                digits[i]+=1
-                if(digits[i]==10):
-                    digits[i]=0
-                elif(digits[i]<10): 
-                    break
-                i-=1
-        return digits
+        num=int("".join(map(str,digits)))
+        return(list(map(int,str(num+1))))
+        
