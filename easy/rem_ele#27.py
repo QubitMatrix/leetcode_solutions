793cea76cb0831b94f67ce5d9715920b323f1e7d
QubitMatrix <preethi13m@gmail.com>
2023-06-21 21:17:14 +0530

    using two pointer method

--- easy/rem_ele#27.py
+++ easy/rem_ele#27.py
@@ -1,7 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-         try:
-                while(1):
-                    nums.remove(val)
-         finally:
-            return len(nums)
+        i=j=0
+        while(i<=j and j<len(nums)):
+            if(nums[j]!=val):
+                if(nums[i]==val):
+                    nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+            j+=1
+        return i
