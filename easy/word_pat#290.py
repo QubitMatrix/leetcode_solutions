038efcd552ecd07f5401324f7191f7d50d9ad66d
QubitMatrix <preethi13m@gmail.com>
2023-07-14 11:37:00 +0530

    Time:O(n), Space:O(n)

--- easy/word_pat#290.py
+++ easy/word_pat#290.py
@@ -1,29 +1,20 @@
+from collections import defaultdict
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        alp=[]
-        encode={}
-        encode_pat={}
-        count=0
+        dic=defaultdict(str)
         s1=""
-        s2=""
-        for x in range(26):
-            alp.append(chr(x+97))
-        words=s.split(" ")
-        for x in words:
-            if(x not in encode.keys()):
-                encode.update({x:alp[count]})
-                count+=1
-        print(encode)
-        count=0
-        for x in pattern:
-            if(x not in encode_pat.keys()):
-                encode_pat.update({x:alp[count]})
-                count+=1
-        print(encode_pat)
-        for x in words:
-            s1+=encode[x]
-        print(s1)
-        for x in pattern:
-            s2+=encode_pat[x]
-        print(s1,s2)
-        return s1==s2
+        l=s.split()
+        if(len(l)!=len(pattern)):
+            return False
+            
+        for x in range(len(l)):
+            if(dic[l[x]]==''):
+                if(pattern[x] in dic.values()):
+                    return False
+                else:
+                    dic[l[x]]=pattern[x]
+            else:
+                if(dic[l[x]]!=pattern[x]):
+                    return False
+            
+        return True
